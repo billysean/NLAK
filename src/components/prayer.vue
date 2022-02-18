75e0325f26696ccade94b985acccf07cd50622da
Billy <billysean.tj@gmail.com>
2022-02-19 00:00:06 +0700

    slight changes

--- src/components/prayer.vue
+++ src/components/prayer.vue
@@ -4,21 +4,17 @@
       <div class="text-center">
         <p
           class="
-            mt-1
-            text-4xl
-            font-extrabold
-            text-gray-900
-            sm:text-5xl sm:tracking-tight
-            lg:text-6xl
+            mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl
           "
         >
-          Need Consulting?
+          Need a Friend?
         </p>
-        <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">
+        <p class="max-w-xl mt-5 mx-auto text-lg text-gray-500">
           There are times where we might be overwhelmed by our situation, and it is alright to share
           your burden. We offer you a personal room for you to share your burden with. Do not worry
           for privacy as we highly value privacy. Your Story ends in that room and with that person.
         </p>
+        <button type="button" class="mt-8 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full shadow-sm text-indigo-500 border-indigo-500 bg-white hover:bg-indigo-100 hover:text-indigo-700 hover:shadow-2xl hover:border-none focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Ask a Friend</button>
       </div>
     </div>
   </div>
