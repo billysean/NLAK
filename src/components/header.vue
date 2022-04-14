c55921c63dae2029cf3dad04e08bcba89ca83469
Billy <billysean.tj@gmail.com>
2022-04-14 11:34:01 +0700

    change ahref to router-link

--- src/components/header.vue
+++ src/components/header.vue
@@ -2,10 +2,10 @@
   <Popover class="relative bg-white">
     <div class="flex justify-between items-center px-4 py-6 sm:px-6 md:justify-start md:space-x-10">
       <div>
-        <a href="/" class="flex">
+        <router-link to="/" class="flex">
           <span class="sr-only">Workflow</span>
           <img class="h-8 w-auto sm:h-10" src="/img/logo.png" alt="" />
-        </a>
+        </router-link>
       </div>
       <div class="-mr-2 -my-2 md:hidden">
         <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -16,15 +16,15 @@
       <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
         <PopoverGroup as="nav" class="flex space-x-10">
           
-          <a href="/selgroup" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sel Group </a>
-          <a href="/findus" class="text-base font-medium text-gray-500 hover:text-gray-900"> Find Us </a>
-          <a href="/media" class="text-base font-medium text-gray-500 hover:text-gray-900"> Media </a>
+          <router-link to="/selgroup" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sel Group </router-link>
+          <router-link to="/findus" class="text-base font-medium text-gray-500 hover:text-gray-900"> Find Us </router-link>
+          <router-link to="/media" class="text-base font-medium text-gray-500 hover:text-gray-900"> Media </router-link>
 
           
         </PopoverGroup>
         <!-- <div class="flex items-center md:ml-12">
-          <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
-          <a href="#" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+          <router-link to="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </router-link>
+          <router-link to="#" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </router-link>
         </div> -->
       </div>
     </div>
@@ -35,9 +35,9 @@
           <div class="pt-5 pb-6 px-5">
             <div class="flex items-center justify-between">
               <div>
-                <a href="/">
+                <router-link to="/">
                   <img class="h-8 w-auto" src="/img/logo.png" alt="Workflow" />
-                </a>
+                </router-link>
               </div>
               <div class="-mr-2">
                 <PopoverButton class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -48,31 +48,31 @@
             </div>
             <div class="mt-6">
               <nav class="grid gap-6">
-                <a href="/selgroup" class="text-base font-medium text-gray-900 hover:text-gray-700"> Sel Group </a>
+                <router-link to="/selgroup" class="text-base font-medium text-gray-900 hover:text-gray-700"> Sel Group </router-link>
 
-              <a href="/findus" class="text-base font-medium text-gray-900 hover:text-gray-700"> Find Us </a>
-              <a href="/media" class="text-base font-medium text-gray-900 hover:text-gray-700"> Media </a>
+              <router-link to="/findus" class="text-base font-medium text-gray-900 hover:text-gray-700"> Find Us </router-link>
+              <router-link to="/media" class="text-base font-medium text-gray-900 hover:text-gray-700"> Media </router-link>
               </nav>
             </div>
           </div>
           <!-- <div class="py-6 px-5">
             <div class="grid grid-cols-2 gap-4">
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Sel Group </a>
+              <router-link to="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Sel Group </router-link>
 
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Find Us </a>
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Media </a>
+              <router-link to="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Find Us </router-link>
+              <router-link to="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Media </router-link>
 
-              <a href="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Enterprise </a>
-              <a v-for="item in resources" :key="item.name" :href="item.href" class="text-base font-medium text-gray-900 hover:text-gray-700">
+              <router-link to="#" class="text-base font-medium text-gray-900 hover:text-gray-700"> Enterprise </router-link>
+              <router-link v-for="item in resources" :key="item.name" :to="item.to" class="text-base font-medium text-gray-900 hover:text-gray-700">
                 {{ item.name }}
-              </a>
+              </router-link>
             </div>
             <div class="mt-6">
-              <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+              <router-link to="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </router-link>
               <p class="mt-6 text-center text-base font-medium text-gray-500">
                 Existing customer?
                 {{ ' ' }}
-                <a href="#" class="text-indigo-600 hover:text-indigo-500"> Sign in </a>
+                <router-link to="#" class="text-indigo-600 hover:text-indigo-500"> Sign in </router-link>
               </p>
             </div>
           </div> -->
